a1fae1f01a8b24908ad8f9dbdfbb6f9802a93615
Aravind Ramachandran <arramac@microsoft.com>
2016-12-09 14:08:55 -0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# documentdb-dotnet-core-getting-started
+---
+services: documentdb
+platforms: dotnet
+author: arramac
+---
+
+# Developing a .NET Core console app using DocumentDB
+This sample shows you how to use the Microsoft Azure DocumentDB service to store and access data from a .NET Core console application.
+
+For a complete end-to-end walkthrough of creating this application, please refer to the [full tutorial on the Azure documentation page](https://azure.microsoft.com/documentation/articles/documentdb-dotnetcore-get-started/).
+
+## Running this sample
+
+1. Before you can run this sample, you must have the following prerequisites:
+* An active Azure account. If you don't have one, you can sign up for a [free account](https://azure.microsoft.com/free/). 
+    * Alternatively, you can use the [Azure DocumentDB Emulator](https://azure.microsoft.com/documentation/articles/documentdb-nosql-local-emulator) for this tutorial.
+* [Visual Studio 2015 Update 3](https://go.microsoft.com/fwlink/?LinkId=691129) and [.NET Core 1.0.1 - VS 2015 Tooling Preview 2](https://go.microsoft.com/fwlink/?LinkID=827546)
+    * If you're working on MacOS or Linux, you can develop .NET Core apps from the command-line by installing the [.NET Core SDK](https://www.microsoft.com/net/core#macos) for the plaform of your choice. 
+    * If you're working on Windows, you can develop .NET Core apps from the command-line by installing the [.NET Core SDK](https://www.microsoft.com/net/core#windows). 
+    * You can use your own editor, or download [Visual Studio Code](https://code.visualstudio.com/) which is free and works on Windows, Linux, and MacOS. 
+
+2. Clone this repository using Git for Windows (http://www.git-scm.com/), or download the zip file.
+
+3. If using the DocumentDB Emulator, please follow instructions at [Azure DocumentDB Emulator](https://azure.microsoft.com/documentation/articles/documentdb-nosql-local-emulator) to install and start the emulator.
+
+If using your Azure DocumentDB account, please substitute the endpoint and authorization key in Program.cs with your account's details.
+
+4. From a command prompt or shell, run `dotnet restore` followed by `dotnet run` to run the sample.
+
+## About the code
+The code included in this sample is intended to get you quickly started with a .NET Core console application that connects to Azure DocumentDB.
+
+## More information
+
+- [Azure DocumentDB Documentation](https://azure.microsoft.com/documentation/services/documentdb/)
+- [Azure DocumentDB .NET SDK](https://www.nuget.org/packages/Microsoft.Azure.DocumentDB/)
+- [Azure DocumentDB .NET SDK Reference Documentation](https://msdn.microsoft.com/library/azure/dn948556.aspx)
---
+++ src/Program.cs
@@ -0,0 +1,383 @@
+﻿//--------------------------------------------------------------------------------- 
+// <copyright file="Program.cs" company="Microsoft">
+// Microsoft (R)  Azure SDK 
+// Software Development Kit 
+//  
+// Copyright (c) Microsoft Corporation. All rights reserved.   
+// 
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,  
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES  
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  
+// </copyright>
+//---------------------------------------------------------------------------------
+
+namespace DocumentDB.GetStarted
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This get-started sample demonstrates the creation of resources and execution of simple queries.  
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "For illustration purposes.")]
+    public class Program
+    {
+        /// <summary>
+        /// The Azure DocumentDB endpoint for running this GetStarted sample.
+        /// </summary>
+        private static readonly string EndpointUrl = "https://localhost:8081";
+
+        /// <summary>
+        /// The authorization key for the Azure DocumentDB account.
+        /// </summary>
+        private static readonly string AuthorizationKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        /// <summary>
+        /// The DocumentDB client instance.
+        /// </summary>
+        private DocumentClient client;
+
+        /// <summary>
+        /// The main method for the demo
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            try
+            {
+                Program p = new Program();
+                p.GetStartedDemo().Wait();
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Run the get started demo for Azure DocumentDB. This creates a database, collection, two documents, executes a simple query 
+        /// and cleans up.
+        /// </summary>
+        /// <returns>The Task for asynchronous completion.</returns>
+        private async Task GetStartedDemo()
+        {
+            // Create a new instance of the DocumentClient
+            this.client = new DocumentClient(new Uri(EndpointUrl), AuthorizationKey);
+
+            await this.CreateDatabaseIfNotExists("FamilyDB");
+
+            await this.CreateDocumentCollectionIfNotExists("FamilyDB", "FamilyCollection");
+
+            // Insert a document, here we create a Family object
+            Family andersenFamily = new Family
+            {
+                Id = "Andersen.1",
+                LastName = "Andersen",
+                Parents = new Parent[] 
+                {
+                    new Parent { FirstName = "Thomas" },
+                    new Parent { FirstName = "Mary Kay" }
+                },
+                Children = new Child[] 
+                {
+                    new Child
+                    {
+                        FirstName = "Henriette Thaulow",
+                        Gender = "female",
+                        Grade = 5,
+                        Pets = new Pet[] 
+                        {
+                            new Pet { GivenName = "Fluffy" }
+                        }
+                    }
+                },
+                District = "WA5",
+                Address = new Address { State = "WA", County = "King", City = "Seattle" },
+                IsRegistered = true
+            };
+
+            await this.CreateFamilyDocumentIfNotExists("FamilyDB", "FamilyCollection", andersenFamily);
+
+            Family wakefieldFamily = new Family
+            {
+                Id = "Wakefield.7",
+                LastName = "Wakefield",
+                Parents = new Parent[]
+                {
+                    new Parent { FamilyName = "Wakefield", FirstName = "Robin" },
+                    new Parent { FamilyName = "Miller", FirstName = "Ben" }
+                },
+                Children = new Child[]
+                {
+                    new Child
+                    {
+                        FamilyName = "Merriam",
+                        FirstName = "Jesse",
+                        Gender = "female",
+                        Grade = 8,
+                        Pets = new Pet[]
+                        {
+                            new Pet { GivenName = "Goofy" },
+                            new Pet { GivenName = "Shadow" }
+                        }
+                    },
+                    new Child
+                    {
+                        FamilyName = "Miller",
+                        FirstName = "Lisa",
+                        Gender = "female",
+                        Grade = 1
+                    }
+                },
+                District = "NY23",
+                Address = new Address { State = "NY", County = "Manhattan", City = "NY" },
+                IsRegistered = false
+            };
+
+            await this.CreateFamilyDocumentIfNotExists("FamilyDB", "FamilyCollection", wakefieldFamily);
+
+            this.ExecuteSimpleQuery("FamilyDB", "FamilyCollection");
+
+            // Clean up/delete the database and client
+            await this.client.DeleteDatabaseAsync(UriFactory.CreateDatabaseUri("FamilyDB"));
+        }
+
+        /// <summary>
+        /// Create a database with the specified name if it doesn't exist. 
+        /// </summary>
+        /// <param name="databaseName">The name/ID of the database.</param>
+        /// <returns>The Task for asynchronous execution.</returns>
+        private async Task CreateDatabaseIfNotExists(string databaseName)
+        {
+            // Check to verify a database with the id=FamilyDB does not exist
+            try
+            {
+                await this.client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(databaseName));
+            }
+            catch (DocumentClientException de)
+            {
+                // If the database does not exist, create a new database
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await this.client.CreateDatabaseAsync(new Database { Id = databaseName });
+                    this.WriteToConsoleAndPromptToContinue("Created {0}", databaseName);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create a collection with the specified name if it doesn't exist.
+        /// </summary>
+        /// <param name="databaseName">The name/ID of the database.</param>
+        /// <param name="collectionName">The name/ID of the collection.</param>
+        /// <returns>The Task for asynchronous execution.</returns>
+        private async Task CreateDocumentCollectionIfNotExists(string databaseName, string collectionName)
+        {
+            try
+            {
+                await this.client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName));
+            }
+            catch (DocumentClientException de)
+            {
+                // If the document collection does not exist, create a new collection
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    DocumentCollection collectionInfo = new DocumentCollection();
+                    collectionInfo.Id = collectionName;
+
+                    // Optionally, you can configure the indexing policy of a collection. Here we configure collections for maximum query flexibility 
+                    // including string range queries. 
+                    collectionInfo.IndexingPolicy = new IndexingPolicy(new RangeIndex(DataType.String) { Precision = -1 });
+
+                    // Optionally, you can configure the partitioning scheme of a collection. Here we configure it to the US District property since we 
+                    // are recording census data for families. You can omit this for collections with low storage  (under 10 GB) and throughput requirements
+                    collectionInfo.PartitionKey.Paths.Add("/District");
+
+                    // DocumentDB collections can be reserved with throughput specified in request units/second. 1 RU is a normalized request equivalent to the read
+                    // of a 1KB document.  Here we create a collection with 400 RU/s. 
+                    await this.client.CreateDocumentCollectionAsync(
+                        UriFactory.CreateDatabaseUri(databaseName),
+                        new DocumentCollection { Id = collectionName },
+                        new RequestOptions { OfferThroughput = 400 });
+
+                    this.WriteToConsoleAndPromptToContinue("Created {0}", collectionName);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create the Family document in the collection if another by the same ID doesn't already exist.
+        /// </summary>
+        /// <param name="databaseName">The name/ID of the database.</param>
+        /// <param name="collectionName">The name/ID of the collection.</param>
+        /// <param name="family">The family document to be created.</param>
+        /// <returns>The Task for asynchronous execution.</returns>
+        private async Task CreateFamilyDocumentIfNotExists(string databaseName, string collectionName, Family family)
+        {
+            try
+            {
+                await this.client.ReadDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, family.Id));
+            }
+            catch (DocumentClientException de)
+            {
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await this.client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), family);
+                    this.WriteToConsoleAndPromptToContinue("Created Family {0}", family.Id);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute a simple query using LINQ and SQL. Here we filter using the "LastName" property.
+        /// </summary>
+        /// <param name="databaseName">The name/ID of the database.</param>
+        /// <param name="collectionName">The name/ID of the collection.</param>
+        private void ExecuteSimpleQuery(string databaseName, string collectionName)
+        {
+            // Set some common query options
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
+
+            // Run a simple query via LINQ. DocumentDB indexes all properties, so queries can be completed efficiently and with low latency.
+            // Here we find the Andersen family via its LastName
+            IQueryable<Family> familyQuery = this.client.CreateDocumentQuery<Family>(
+                UriFactory.CreateDocumentCollectionUri(databaseName, collectionName), queryOptions)
+                .Where(f => f.LastName == "Andersen");
+
+            // The query is executed synchronously here, but can also be executed asynchronously via the IDocumentQuery<T> interface
+            Console.WriteLine("Running LINQ query...");
+            foreach (Family family in familyQuery)
+            {
+                Console.WriteLine("\tRead {0}", family);
+            }
+
+            // Now execute the same query via direct SQL
+            IQueryable<Family> familyQueryInSql = this.client.CreateDocumentQuery<Family>(
+                UriFactory.CreateDocumentCollectionUri(databaseName, collectionName),
+                "SELECT * FROM Family WHERE Family.lastName = 'Andersen'",
+                queryOptions);
+
+            Console.WriteLine("Running direct SQL query...");
+            foreach (Family family in familyQuery)
+            {
+                Console.WriteLine("\tRead {0}", family);
+            }
+        }
+
+        /// <summary>
+        /// Write to the console, and prompt to continue.
+        /// </summary>
+        /// <param name="format">The string to be displayed.</param>
+        /// <param name="args">Optional arguments.</param>
+        private void WriteToConsoleAndPromptToContinue(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// A Family class, e.g. storing census data about families within the United States. We use this data model throughout the 
+        /// sample to show how you can store objects within your application logic directly as JSON within Azure DocumentDB. 
+        /// </summary>
+        public class Family
+        {
+            [JsonProperty(PropertyName = "id")]
+            public string Id { get; set; }
+
+            public string LastName { get; set; }
+
+            public string District { get; set; }
+
+            public Parent[] Parents { get; set; }
+
+            public Child[] Children { get; set; }
+
+            public Address Address { get; set; }
+
+            public bool IsRegistered { get; set; }
+
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+
+        /// <summary>
+        /// A parent class used within Family
+        /// </summary>
+        public class Parent
+        {
+            public string FamilyName { get; set; }
+
+            public string FirstName { get; set; }
+        }
+
+        /// <summary>
+        /// A child class used within Family
+        /// </summary>
+        public class Child
+        {
+            public string FamilyName { get; set; }
+
+            public string FirstName { get; set; }
+
+            public string Gender { get; set; }
+
+            public int Grade { get; set; }
+
+            public Pet[] Pets { get; set; }
+        }
+
+        /// <summary>
+        /// A pet class that belongs to a Child
+        /// </summary>
+        public class Pet
+        {
+            public string GivenName { get; set; }
+        }
+
+        /// <summary>
+        /// An address class containing data attached to a Family.
+        /// </summary>
+        public class Address
+        {
+            public string State { get; set; }
+
+            public string County { get; set; }
+
+            public string City { get; set; }
+        }
+    }
+}
